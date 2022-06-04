59e6b6a489b8064ac89cc2eb41946d1856d6d763
joaquingomez1 <joaquingomez3098@gmail.com>
2022-06-04 19:35:03 -0300

    fixed wrong type on protected request

--- libs/protectedRequest.ts
+++ libs/protectedRequest.ts
@@ -1,11 +1,12 @@
 import { NextPageContext } from "next";
+import AppResponse from "../interfaces/appResponse";
 import requestWithCredentials from "./authRequest";
 
 export default async function protectedRequest(
   ctx: NextPageContext,
   redirectUrl: string
 ) {
-  const result = await requestWithCredentials(ctx, "/user");
+  const result = await requestWithCredentials<AppResponse>(ctx, "/user");
 
   if (!result || result.message) {
     return {
